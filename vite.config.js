5f95ef8a78b868d3da0781ace28bf40090988fd0
Td1199888 <tommy@dinhcreative.com>
2026-05-26 12:15:45 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
