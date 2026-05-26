4e7690c7ddf54a62184bc3d8b1bc4aa454136cbb
Td1199888 <tommy@dinhcreative.com>
2026-05-26 13:29:31 +0200

    Add error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) { super(props); this.state = { error: null, info: null }; }
+  componentDidCatch(error, info) { this.setState({ error, info }); }
+  static getDerivedStateFromError(error) { return { error }; }
+  render() {
+    if (this.state.error) {
+      return React.createElement('div', { style: { padding: 40, fontFamily: 'monospace', background: '#fff', color: 'red' } },
+        React.createElement('h2', null, 'App Error'),
+        React.createElement('pre', null, String(this.state.error)),
+        React.createElement('pre', null, this.state.error?.stack?.substring(0, 800))
+      );
+    }
+    return this.props.children;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement(ErrorBoundary, null,
+    React.createElement(App)
+  )
 )
